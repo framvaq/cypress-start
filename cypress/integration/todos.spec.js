1d31d0ae84018781c2d3f3dbc7abf45c7902920f
Usuario invitado <Guest@jon-mac.local>
2020-05-28 12:34:04 +0200

    mocked backend

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,6 +1,24 @@
 describe("Todo app", () => {
   it("loads the page", () => {
+    cy.server();
+    //the route is in the xhr request in cypress
+    cy.route("/api/todos",
+      [
+        {
+          text: "Hello world",
+          completed: false,
+          id: 3,
+        },
+        {
+          id: 4,
+          completed: true,
+          text: "Goodnight moon",
+        },
+      ]).as('preload');
+
     cy.visit("/");
+
+    cy.wait('@preload');
 
     cy.get("[data-cy=todo-item-3]")
       .should("have.text", "Hello world")
