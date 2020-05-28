40928d279f03c34d4d055c4c80e9ad4e79c97042
Usuario invitado <Guest@jon-mac.local>
2020-05-28 13:32:38 +0200

    stores

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,17 +1,38 @@
-describe("Todo app", () => {
-  it("loads the page", () => {
-    cy.visit("/");
+describe('Todo app', () => {
+  it('loads the page', () => {
+    cy.visit('/');
 
-    cy.get("[data-cy=todo-item-3]")
-      .should("have.text", "Hello world")
-      .should("not.have.class", "completed")
-      .find(".toggle")
-      .should("not.be.checked");
+    //cy.window().then(($window) => { console.log($window.store) });
+    cy.window()
+      .its('store')
+      .invoke('getState')
+      .should('deep.equal', {
+        todos: [
+          {
+            text: 'Hello world',
+            completed: false,
+            id: 3,
+          },
+          {
+            id: 4,
+            completed: true,
+            text: 'Goodnight moon',
+          },
+        ],
 
-    cy.get("[data-cy=todo-item-4]")
-      .should("have.text", "Goodnight moon")
-      .should("have.class", "completed")
-      .find(".toggle")
-      .should("be.checked");
+        visibilityFilter: 'show_all',
+      });
+
+    cy.get('[data-cy=todo-item-3]')
+      .should('have.text', 'Hello world')
+      .should('not.have.class', 'completed')
+      .find('.toggle')
+      .should('not.be.checked');
+
+    cy.get('[data-cy=todo-item-4]')
+      .should('have.text', 'Goodnight moon')
+      .should('have.class', 'completed')
+      .find('.toggle')
+      .should('be.checked');
   });
 });
