40507c8e09426c4cf908ebe9604f5d67f90d9275
Usuario invitado <Guest@jon-mac.local>
2020-05-28 13:06:52 +0200

    faked update and create

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,24 +1,29 @@
 describe("Todo app", () => {
   it("loads the page", () => {
     cy.server();
+
+    /*
+  Mock the route to create a new todo, and reply with a 200
+  Mock the route to edit an existing todo, and reply with a 200
+*/
+
     //the route is in the xhr request in cypress
-    cy.route("/api/todos",
-      [
-        {
-          text: "Hello world",
-          completed: false,
-          id: 3,
-        },
-        {
-          id: 4,
-          completed: true,
-          text: "Goodnight moon",
-        },
-      ]).as('preload');
+    cy.route("/api/todos", [
+      {
+        text: "Hello world",
+        completed: false,
+        id: 3,
+      },
+      {
+        id: 4,
+        completed: true,
+        text: "Goodnight moon",
+      },
+    ]).as("preload");
 
     cy.visit("/");
 
-    cy.wait('@preload');
+    cy.wait("@preload");
 
     cy.get("[data-cy=todo-item-3]")
       .should("have.text", "Hello world")
@@ -31,5 +36,62 @@ describe("Todo app", () => {
       .should("have.class", "completed")
       .find(".toggle")
       .should("be.checked");
+
+    cy.route("/api/todos", [
+      {
+        text: "Hello Mars",
+        completed: false,
+        id: 3,
+      },
+      {
+        text: "Goodnight moon",
+        completed: true,
+        id: 4,
+      },
+      {
+        text: "sfsdfsd",
+        completed: false,
+        id: 5,
+      },
+    ]).as("modified");
+
+    cy.visit("/");
+
+    cy.wait("@modified");
+
+    cy.get("[data-cy=todo-item-3]")
+      .should("have.text", "Hello Mars");
+    
+      cy.route("/api/todos", [
+      {
+        text: "Hello Mars",
+        completed: false,
+        id: 3,
+      },
+      {
+        text: "Goodnight moon",
+        completed: true,
+        id: 4,
+      },
+      {
+        text: "sfsdfsd",
+        completed: false,
+        id: 5,
+      },
+      {
+        text:"new one",
+        completed: true,
+        id: 6,
+      }
+    ]).as("added");
+
+    cy.visit("/");
+
+    cy.wait("@added");
+
+    cy.get("[data-cy=todo-item-6]")
+      .should("have.text", "new one");
+
+
   });
 });
